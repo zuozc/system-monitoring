4b0b94eaa2dc6997a523254abf091a0bec9e9388
Loyal Zuo <zzczuo@gmail.com>
2015-07-23 16:19:03 +0800

    Add a new logic for checking whether disks can be write.

--- disk_usage.sh
+++ disk_usage.sh
@@ -14,6 +14,39 @@ record_log() {
 
 }
 
+check_disk_2() {
+
+	(df -h | grep '^/dev') | while read line
+	do 
+		
+		disk=$(echo $line | awk '{print $1}')
+		usage=$(echo $line | awk '{print $5}')
+		forder=$(echo $line | awk '{print $6}')
+			
+		# initiate
+		disk=${disk##*\/}
+		writable=1
+		readable=1
+		usage=${usage%%\%}
+    
+		# writable or not
+    touch ${forder}/rwtest0
+    if [[ $? -eq 0 ]]; then
+      writable=0
+      rm ${forder}/rwtest0
+    fi
+		# writable or not
+		#if [[ -w ${forder} ]]; then writable=0; fi
+		
+		# readable or not
+		if [[ -r ${forder} ]]; then readable=0;	fi
+		
+		record_log $disk $writable $readable $usage
+		
+	done
+  
+}
+
 # check if disk is writable, readable or not and its usage.
 check_disk() {
  
